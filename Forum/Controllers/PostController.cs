74c73b861e9a87d8fa96d37f71b2993ae6d62dda
Vlad <name@am>
2024-06-24 20:57:29 +0300

    добавил контролер для постов

--- Forum/Controllers/PostController.cs
+++ Forum/Controllers/PostController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Forum.Controllers
 {
@@ -18,17 +19,61 @@ namespace Forum.Controllers
         [HttpGet]
         public async Task<IActionResult> GetPost(int id)
         {
-            Post? p=null;
-            await Task.Run(() => { p = _context.Posts.FirstOrDefault(i => i.PostId == id); });
-            if (p != null ) return Ok(p);
+            Post? post=null;
+            await Task.Run(() => { post = _context.Posts.FirstOrDefault(i => i.PostId == id); });
+            if (post != null ) return Ok(post);
             return NotFound();
         }
 
         [HttpPost]
-        public async void CreatePost(Post post)
+        public async Task<IActionResult> CreatePost(Post post)//без картинки 
         {
-            await _context.Posts.AddAsync(post);         
+            await _context.Posts.AddAsync(post);
+            _context.SaveChanges();
+            return NoContent();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUserPosts(int userID)
+        {
+            List<Post>? res = null;
+            await Task.Run(() => { res = _context.Users.FirstOrDefault(i => i.UserId == userID)?.Posts.ToList(); });
+            if (res != null) return Ok(res);
+            return NotFound();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetPostComents(int postId)
+        {
+            List<Comment>? comments=null;
+            await Task.Run(() => { comments = _context.Posts.FirstOrDefault(i => i.PostId == postId)?.Comments.ToList(); });
+            if(comments != null) return Ok(comments);
+            return NotFound();
         }
 
-    }
+        [HttpPut]
+        public async Task<IActionResult> UpdatePost(Post newPost)
+        {
+            var post = _context.Posts.FirstOrDefault(i => i.PostId == newPost.PostId);
+            if(post != null) 
+            {
+                post.PostSubtitle = newPost.PostSubtitle;
+                post.PostTitle = newPost.PostTitle;
+                post.PostBody = newPost.PostBody;
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+            return NotFound();          
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DelatePost(int postId)
+        {
+             var post = await _context.Posts.FindAsync(postId);
+            if(post != null)
+            {
+                _context.Remove(post);
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+            return NotFound();
+        }
+    }  
 }
